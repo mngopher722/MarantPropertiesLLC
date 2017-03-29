8578dfb241ef7a6ede227957b73f1e3811e91495
mngopher722 <ryan.feeney@mymail.nwtc.edu>
2017-03-29 11:22:24 -0500

    Add project files.

---
+++ MarantPropertiesLLC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarantPropertiesLLC.MvcApplication" Language="C#" %>
---
+++ MarantPropertiesLLC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarantPropertiesLLC.Startup))]
+namespace MarantPropertiesLLC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
