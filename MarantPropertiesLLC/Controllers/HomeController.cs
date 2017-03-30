dbb88eccae38d3a51f02dd4635b1a71785e233f5
mngopher722 <ryan.feeney@mymail.nwtc.edu>
2017-03-30 12:20:06 -0500

    Added Image Folder under Content Added GB Waterfront Picture to folder and bootstrap.css Updated HomeController Viewbag messages Updated Contact information on Contact.cshtml

--- MarantPropertiesLLC/Controllers/HomeController.cs
+++ MarantPropertiesLLC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MarantPropertiesLLC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Information";
 
             return View();
         }
