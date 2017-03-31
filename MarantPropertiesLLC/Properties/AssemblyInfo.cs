540db45bb0959b6736c2c4ebf547b104067d813c
mngopher722 <ryan.feeney@mymail.nwtc.edu>
2017-03-31 14:55:50 -0500

    Updated Comment Line in AssemblyInfo

--- MarantPropertiesLLC/Properties/AssemblyInfo.cs
+++ MarantPropertiesLLC/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+// Site By Ryan Feeney
 [assembly: AssemblyTitle("MarantPropertiesLLC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
